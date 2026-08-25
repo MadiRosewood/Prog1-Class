49a20d351c8534553c71f756af30cffb7ea74f25
MadiRosewood <Madison.Rosewood.anderson@gmail.com>
2026-08-25 10:38:14 -0400

    Create chien.py
    Version1

---
+++ chien.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print(" ")
+print("Get all triangle faces")
+import math
+from fractions import Fraction
+h = 2
+w = 3
+sqH = h**2
+sqW = w**2
+s = math.sqrt(sqH + sqW)
+str(Fraction(s))
+print("Height:", h)
+print("Width:", w)
+print("Triangle Diagonal is", s, Fraction(s))
