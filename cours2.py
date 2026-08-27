9e3ae539373f6ec5262f38a802e6af58e0b444a5
Madison <madison.rosewood.anderson@gmail.com>
2026-08-27 16:58:39 -0400

    Update cours2.py

--- cours2.py
+++ cours2.py
@@ -1,1 +1,25 @@
-print("test")
+# Auteur    : Madison Rosewood
+# Date      : 08/27/26
+# Sujet     : Demo du cours #2
+
+a = 10
+b = 20
+c = a + b
+print(c)
+
+nombre1 = 2; nombre2 = 4; nombre3 = 7
+nombre4 = 5; nombre5 = 3; nombre6 = 3
+nombre7 = 2; nombre8 = 1; nombre9 = 8
+
+total = (
+    nombre1 + nombre2 + nombre3 \
+        + nombre4 + nombre5 + nombre6\
+            + nombre7 + nombre8 + nombre9
+)
+
+total2 = (
+    nombre1 + nombre2 + nombre3
+    + nombre4 + nombre5 + nombre6
+    + nombre7 + nombre8 + nombre9
+)
+print(total)
